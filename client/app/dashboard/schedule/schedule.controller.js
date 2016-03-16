3e75e3cbd24b90eef78557e9b85e39e10adb3d96
bhuber2010 <bhuber@alumni.colostate.edu>
2016-03-16 12:25:45 -0600

    initial booking JS code put in

--- client/app/dashboard/schedule/schedule.controller.js
+++ client/app/dashboard/schedule/schedule.controller.js
@@ -18,6 +18,18 @@ class ScheduleComponent {
         };
       })
 
+    this.widget = new TimekitBooking();
+    this.widget.init({
+      name: 'Brian',
+      email: 'bhuber@alumni.colostate.edu',
+      apiToken: 'S2oK9czO8wdgQjGFvQxgufWaJgs3rkSL',
+      calendar: '7e633362-716a-4f23-bb35-9fcda204b1a4',
+      avatar: 'https://lh6.googleusercontent.com/-ePoHAHPfVgE/AAAAAAAAAAI/AAAAAAAAF-Q/beBK7juwGZ0/photo.jpg',
+      timekitFindTime: {
+        ignore_all_day_events: true
+      }
+    })
+
   }
 }
 
