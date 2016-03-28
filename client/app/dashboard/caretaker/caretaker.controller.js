e5b46ef79b46171cea8f1cefcebaa90310dcb887
bhuber2010 <bhuber@alumni.colostate.edu>
2016-03-27 20:51:48 -0700

    put dogs on the map & add toggle map btn

--- client/app/dashboard/caretaker/caretaker.controller.js
+++ client/app/dashboard/caretaker/caretaker.controller.js
@@ -2,13 +2,37 @@
 (function(){
 
 class CaretakerComponent {
-  constructor(dashdata, DogData, $stateParams, lodash) {
+  constructor($window, $timeout, dashdata, DogData, $state, $stateParams, lodash) {
+    this.lodash = lodash;
+    this.GMaps = $window.GMaps;
     this.dashdata = dashdata;
     this.DogData = DogData;
+    this.$timeout = $timeout;
     this.$stateParams = $stateParams;
+    this.$state = $state;
     this.selectedBreed = '';
     this.selectedCity = '';
     this.selectedState = '';
+    this.mapViewToggle = false;
+
+    this.map = new this.GMaps({
+      div: '#map',
+      lat: 39.7392360,
+      lng: -104.9902510,
+      zoom: 12
+    });
+
+    this.GMaps.geolocate({
+      success: position => {
+        this.map.setCenter(position.coords.latitude, position.coords.longitude);
+      },
+      error: error => {
+        alert('Geolocation failed: ', error.message);
+      },
+      not_supported: () => {
+        alert("Your browser does not support geolocation");
+      }
+    });
 
     this.DogData.getAllDogs().$promise
       .then(doglist => {
@@ -31,13 +55,82 @@ class CaretakerComponent {
           cats.breeds.push(dog.breed.toUpperCase());
           cats.cities.push(dog.owner_user.city.toUpperCase());
           cats.states.push(dog.owner_user.state.toUpperCase());
+
+          this.mapResults(dog, this.$stateParams.activity)
+
         })
 
         this.breeds = lodash(cats.breeds).uniq().value();
         this.cities = lodash(cats.cities).uniq().value();
         this.states = lodash(cats.states).uniq().value();
 
+        return this.map;
       })
+      .then((map) => {
+        console.log(this.map);
+      })
+
+  }
+
+  toggleMap() {
+    this.mapViewToggle = !this.mapViewToggle;
+    console.log(this.map);
+    this.map.refresh();
+  }
+
+  selectedStateChanged() {
+    this.map.removeMarkers();
+    if (this.selectedState) {
+      var filteredDogs = this.lodash.filter(this.dogs, dog => {
+        return dog.owner_user.state.toLowerCase() == this.selectedState.toLowerCase()
+      });
+      filteredDogs.forEach((dog, i) => {
+        this.mapResults(dog, this.$stateParams.activity);
+        if (filteredDogs.length - 1 == i) {
+          console.log(this.map);
+          // this.map.fitZoom();
+        }
+      })
+    } else {
+      this.dogs.forEach((dog, i) => {
+        this.mapResults(dog, this.$stateParams.activity);
+        if (this.dogs.length - 1 == i) {
+          console.log(this.map);
+          // this.map.fitZoom();
+        }
+      })
+    }
+  }
+
+  mapResults(dogObj, activity) {
+    this.GMaps.geocode({
+      address: `${dogObj.owner_user.address}, ${dogObj.owner_user.city}, ${dogObj.owner_user.state}`,
+      callback: (results, status) => {
+        if (status == 'OK') {
+          var latlng = results[0].geometry.location;
+          this.map.addMarker({
+            lat: latlng.lat(),
+            lng: latlng.lng(),
+            title: dogObj.name,
+            infoWindow: {
+              content: `Name: ${dogObj.name}<br> Breed: ${dogObj.breed}`
+            },
+            icon : {
+              url : '../../../assets/images/dog_icon.png'
+            },
+            click:(e) => {
+              this.$state.go('schedule', {id: dogObj._id, activity: activity})
+            },
+            mouseover: function(e) {
+              this.infoWindow.open(this.map, this);
+            },
+            mouseout: function(e) {
+              this.infoWindow.close();
+            }
+          });
+        }
+      }
+    });
   }
 
 }
